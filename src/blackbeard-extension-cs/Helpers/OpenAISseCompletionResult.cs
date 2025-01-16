19941601b4bac853f05b057a3a2598fb667bb9ac
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2025-01-16 20:37:06 +0000

    Second attempt at converting to use MEAI.

---
+++ src/blackbeard-extension-cs/Helpers/OpenAISseCompletionResult.cs
@@ -0,0 +1,25 @@
+﻿namespace Microsoft.Extensions.AI;
+
+public sealed class OpenAISseCompletionResult(IAsyncEnumerable<StreamingChatCompletionUpdate> streamingUpdates) : IResult
+{
+    public async Task ExecuteAsync(HttpContext ctx)
+    {
+        ctx.Response.StatusCode = StatusCodes.Status200OK;
+        ctx.Response.ContentType = "application/json";
+        streamingUpdates = streamingUpdates.Select(update =>
+        {
+            update.ModelId ??= "<unspecified>";
+            return update;
+        });
+
+        await OpenAISerializationHelpers.SerializeStreamingAsync(ctx.Response.Body, streamingUpdates, cancellationToken: ctx.RequestAborted).ConfigureAwait(false);
+    }
+}
+
+public static class OpenAISseCompletionResultExtensions
+{
+    public static IResult ToOpenAISseResult(this IAsyncEnumerable<StreamingChatCompletionUpdate> streamingUpdates)
+    {
+        return new OpenAISseCompletionResult(streamingUpdates);
+    }
+}
--- src/blackbeard-extension-cs/Program.cs
+++ src/blackbeard-extension-cs/Program.cs
@@ -1,45 +1,41 @@
-using Microsoft.AspNetCore.Mvc;
-
-const string GithubCopilotCompletionsUrl = "https://api.githubcopilot.com/chat/completions";
+using Microsoft.Extensions.AI;
+using System.ComponentModel;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpClient();
+builder.Services.AddScoped<IChatClient>(services =>
+    new GithubChatClient(services, modelId: "gpt-4")
+        .AsBuilder()
+        .UseFunctionInvocation()
+        .Build());
+    
+builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 app.MapGet("/", () => "Ahoy, matey! Welcome to the Blackbeard Pirate GitHub Copilot Extension!");
-app.MapPost("/", async (
-    [FromHeader(Name = "X-GitHub-Token")] string githubToken,
-    ChatCompletionRequest chatCompletionRequest,
-    HttpClient httpClient,
-    CancellationToken cancellationToken) =>
+app.MapPost("/", (
+    OpenAIChatCompletionRequest chatCompletionRequest,
+    IChatClient chatClient) =>
 {
-    chatCompletionRequest.Stream = true;
+    (chatCompletionRequest.Options.Tools ??= []).Add(AIFunctionFactory.Create(GetNearestPorts));
     chatCompletionRequest.Messages.Insert(0, new ChatMessage
     {
-        Role = "system",
-        Content = "Concisely reply as if you were Blackbeard the Pirate."
+        Role = ChatRole.System,
+        Contents = [new TextContent("Concisely reply as if you were Blackbeard the Pirate.")]
     });
 
-    HttpRequestMessage httpRequest = new(HttpMethod.Post, GithubCopilotCompletionsUrl)
-    {
-        Headers = { Authorization = new("Bearer", githubToken) },
-        Content = JsonContent.Create(chatCompletionRequest),
-    };
-
-    var copilotLLMResponse = await httpClient.SendAsync(httpRequest, cancellationToken);
-    return Results.Stream(await copilotLLMResponse.Content.ReadAsStreamAsync(), "application/json");
+    return chatClient.CompleteStreamingAsync(chatCompletionRequest.Messages, chatCompletionRequest.Options)
+        .ToOpenAISseResult();
 });
 
 app.Run();
 
-public record ChatCompletionRequest
+[Description("Scouted ports and their treasure.")]
+IEnumerable<Port> GetNearestPorts()
 {
-    public List<ChatMessage> Messages { get; set; } = [];
-    public bool Stream { get; set; }
+    yield return new Port("Port Royal", GarrisonSize: 25, Treasure: 2000);
+    yield return new Port("Nassau", GarrisonSize: 100, Treasure: 10_000);
+    yield return new Port("Tortuga", GarrisonSize: 30, Treasure: 2000);
+    yield return new Port("Havana", GarrisonSize: 10, Treasure: 4000);
 }
 
-public record ChatMessage
-{
-    public required string Role { get; set; }
-    public required string Content { get; set; }
-}
+record Port(string Name, int GarrisonSize, int Treasure);
