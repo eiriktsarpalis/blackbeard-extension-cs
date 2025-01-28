f72de5f1f29547c3e2c3f86abd74e58693dab6b0
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2025-01-28 11:52:39 +0000

    Add demo scaffolding.

---
+++ src/blackbeard-extension-cs/Helpers/OpenAISseCompletionResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Microsoft.Extensions.AI;
+
+public sealed class OpenAISseCompletionResult(IAsyncEnumerable<StreamingChatCompletionUpdate> streamingUpdates) : IResult
+{
+    public async Task ExecuteAsync(HttpContext ctx)
+    {
+        ctx.Response.StatusCode = StatusCodes.Status200OK;
+        ctx.Response.ContentType = "application/json";
+        streamingUpdates = streamingUpdates.Select(update =>
+        {
+            update.ModelId ??= "<unspecified>";
+            return update;
+        });
+
+        await OpenAISerializationHelpers.SerializeStreamingAsync(ctx.Response.Body, streamingUpdates, cancellationToken: ctx.RequestAborted).ConfigureAwait(false);
+    }
+}
